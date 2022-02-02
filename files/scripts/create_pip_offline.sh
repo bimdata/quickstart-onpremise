30c2d282c499b84e98e3c7c98e164b26571980af
Romain <58464216+RomainMou@users.noreply.github.com>
2022-02-02 16:10:09 +0100

    Feat/add centos and offline support (#14)
    * Add basic centos support.
    
    * Add offline install.
    
    * Fix typos and bad english.
    
    * Fix pip offline for debian.

---
+++ files/scripts/create_pip_offline.sh
@@ -0,0 +1,31 @@
+#! /usr/bin/bash
+
+set -euo pipefail
+
+WORKING_DIR="/tmp/bimdata"
+
+REQUIREMENTS=$(cat <<EOF
+pip
+docker-compose==1.29.2
+EOF
+)
+
+echo -n "Creating needed virtualenv…"
+mkdir -p "${WORKING_DIR}/pip-archives"
+echo "${REQUIREMENTS}" > "${WORKING_DIR}/requirements.txt"
+virtualenv -p python3 "${WORKING_DIR}/venv"
+echo "Ok."
+
+echo -n "Updating pip if needed…"
+"${WORKING_DIR}/venv/bin/pip" install --disable-pip-version-check --upgrade pip > /dev/null
+echo "Ok."
+
+echo -n "Downloading prerequisites…"
+"${WORKING_DIR}/venv/bin/pip" download -d "${WORKING_DIR}/pip-archives" -r "${WORKING_DIR}/requirements.txt" > /dev/null
+echo "Ok."
+
+echo -n "Creating the archive…"
+tar -cjf "$WORKING_DIR/pip-archives.tar.bz2" -C "${WORKING_DIR}" pip-archives
+echo -e "Ok.\n"
+
+echo "You can now retrieve the '$WORKING_DIR/pip-archives.tar.bz2' file and put it in your ansible inventory."
