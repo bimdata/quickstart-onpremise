6161768ed8c976f96332b090f94130acb8aa8759
RomainMou <58464216+RomainMou@users.noreply.github.com>
2022-06-28 16:23:04 +0200

    Update on-premises versions.

--- files/scripts/retag_images.sh
+++ files/scripts/retag_images.sh
@@ -3,21 +3,15 @@
 set -euo pipefail
 
 src_repos=docker-registry.bimdata.io/on-premises
-src_tag=20211208
+src_tag=20220624
 
 dest_repos=
 dest_tag=
 
-img_list=(api connect platform platform_back iam documentation archive workers viewer_360 xkt_worker)
+img_list=(api connect platform platform_back iam documentation archive marketplace_back marketplace workers viewer_360 xkt_worker)
 
 for img in ${img_list[@]} ; do
   sudo docker pull ${src_repos}/${img}:${src_tag}
   sudo docker tag ${src_repos}/${img}:${src_tag} ${dest_repos}/${img}:${dest_tag}
   sudo docker push ${dest_repos}/${img}:${dest_tag}
 done
-
-# Special image needed for migration
-migration_img="api:apps_from_connect_to_api"
-sudo docker pull ${src_repos}/${migration_img}
-sudo docker tag ${src_repos}/${migration_img} ${dest_repos}/${migration_img}
-sudo docker push ${dest_repos}/${migration_img}
