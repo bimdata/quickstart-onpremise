7f1bc9c232dcf5f53da5584c181ed996b002ba44
RomainMou <58464216+RomainMou@users.noreply.github.com>
2023-03-15 21:02:37 +0100

    Improve s3 migration script.

--- files/scripts/local_to_s3.sh
+++ files/scripts/local_to_s3.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/bash
 
-# This is a really naive script without any optimization
-
-# Object storage config
+# Object storage
 s3_endpoint_url=""
 s3_region_name=""
 s3_access_key_id=""
@@ -12,14 +10,40 @@ s3_bucket_name=""
 # Storage path
 local_storage_path="/opt/bimdata/data/web/api_storage"
 
+# Other configs
+gzip_content_types="
+  text/plain
+  application/json
+  application/octet-stream
+  image/svg+xml
+  text/xml
+  model/gltf+json
+  application/p21
+  application/pdf
+  application/msword
+  application/vnd.ms-excel
+  text/csv
+"
+
+force_default_content_types="
+  application/p21
+"
 export AWS_ACCESS_KEY_ID=${s3_access_key_id}
 export AWS_SECRET_ACCESS_KEY=${s3_secret_access_key}
 
-# For all file in the local storage folder, we compute retrieve the full path
-# The we compute the corresponding s3 key by removing the useless part
-# Then we copy it with the AWS cli to the configured bucket
 find ${local_storage_path} -type f | while read line ; do
   file=${line}
   s3_key=${line#"$local_storage_path"}
-  aws --endpoint-url ${s3_endpoint_url} --region ${s3_region_name} s3 cp ${file} s3://${s3_bucket_name}${s3_key}
+  file_mime_type=$(file --brief --mime-type $file)
+
+  # Some content types need to be change, because of firefox behavior when downloaded
+  if [[ "$(echo $force_default_content_types)" =~ ( |^)$file_mime_type( |$) ]] ; then
+    file_mime_type="application/octet-stream"
+  fi
+
+  if [[ "$(echo $gzip_content_types)" =~ ( |^)$file_mime_type( |$) ]] ; then
+    gzip -c ${file} | aws --endpoint-url ${s3_endpoint_url} --region ${s3_region_name} s3 cp - s3://${s3_bucket_name}${s3_key} --content-type ${file_mime_type} --content-encoding 'gzip'
+  else
+    aws --endpoint-url ${s3_endpoint_url} --region ${s3_region_name} s3 cp ${file} s3://${s3_bucket_name}${s3_key} --content-type ${file_mime_type}
+  fi
 done
