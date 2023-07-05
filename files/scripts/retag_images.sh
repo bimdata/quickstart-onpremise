c8d3c65f873bdd5a9deaab1c8a2885489f058a81
RomainMou <58464216+RomainMou@users.noreply.github.com>
2023-07-05 10:10:25 +0200

    Fix typo.

--- files/scripts/retag_images.sh
+++ files/scripts/retag_images.sh
@@ -8,7 +8,7 @@ src_tag=20230608
 dest_repos=
 dest_tag=
 
-img_list=(api connect platform platform_back iam documentation archive workers viewer_360 xkt_worker xkt_v10_worker wg_worker image_preview_worker pdf_preview_worker pointcloud_worker)
+img_list=(api connect platform platform_back iam documentation archive workers viewer_360 xkt_worker xkt_v10_worker dwg_worker image_preview_worker pdf_preview_worker pointcloud_worker)
 
 for img in ${img_list[@]} ; do
   sudo docker pull ${src_repos}/${img}:${src_tag}
