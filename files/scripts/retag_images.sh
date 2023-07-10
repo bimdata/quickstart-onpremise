b0ffc9e23cd1823403b4f95857e8f796ba630bdc
RomainMou <58464216+RomainMou@users.noreply.github.com>
2023-07-10 14:42:48 +0200

    Update docker images in the sample inventory.

--- files/scripts/retag_images.sh
+++ files/scripts/retag_images.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 src_repos=docker-registry.bimdata.io/on-premises
-src_tag=20230608
+src_tag=20230710
 
 dest_repos=
 dest_tag=
 
-img_list=(api connect platform platform_back iam documentation archive workers viewer_360 xkt_worker xkt_v10_worker dwg_worker image_preview_worker pdf_preview_worker pointcloud_worker)
+img_list=(api connect platform platform_back marketplace marketplace_back iam documentation archive workers viewer_360 xkt_worker xkt_v10_worker dwg_worker image_preview_worker pdf_preview_worker pointcloud_worker)
 
 for img in ${img_list[@]} ; do
   sudo docker pull ${src_repos}/${img}:${src_tag}
