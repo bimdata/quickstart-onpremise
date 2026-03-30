47e3e0506b904bf53b42c555711dcc87bbf616a4
RomainMou <58464216+RomainMou@users.noreply.github.com>
2026-03-30 15:45:12 +0200

    Bump version

--- files/scripts/create_docker_offline.sh
+++ files/scripts/create_docker_offline.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 archive_path="./files/offline/docker"
 
 src_private_repos=docker-registry.bimdata.io/on-premises
-src_private_tag=20260309
+src_private_tag=20260330
 
 app_images=(
   rabbitmq:3.12-management-alpine
